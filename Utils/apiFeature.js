5c081130a7380ce3a940c8ec52378108ebf14c57
Saurav-Navdhare <navdharesaurav2003@gmail.com>
2023-07-10 14:20:39 +0530

    Added API features

--- Utils/apiFeature.js
+++ Utils/apiFeature.js
@@ -24,3 +24,25 @@ export const ConnectWallet = async () => {
         console.log(error)
     }
 }
+
+const fetchContract = (signerOnProvider) => 
+    new ethers.Contract(ChatAppABI, ChatAppAddress, signerOnProvider)
+
+export const connectingWithContract = async () => {
+    try {
+        const web3modal = new Web3Modal()
+        const connection = await web3modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
+        const contract = fetchContract(signer)
+        return contract
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const converTime = (time) => {
+    const date = new Date(time.toNumber())
+    const realTime = date.getHours() + "/" + date.getMinutes() + "/" + date.getSeconds() + " Date: " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    return realTime
+}
